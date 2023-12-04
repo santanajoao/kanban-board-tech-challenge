3c1879d5251c181877ed3ad6b6af39c7059e81a2
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-12-04 20:47:18 -0300

    feat: create mobile header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,21 @@
 import { LuMenu } from 'react-icons/lu';
+import { K2D } from 'next/font/google';
+
+const k2d = K2D({ subsets: ['latin'], weight: ['700'] });
 
 export default function Home() {
   return (
     <div className="">
-      <header>
-        <button type="button">
+      <header className="flex bg-primaryPurple px-3 py-2 text-white">
+        <button type="button" className="text-2xl">
           <LuMenu />
         </button>
 
-        <h1>Taskban</h1>
+        <h1 className={`text-2xl ${k2d.className} w-full text-center mr-6`}>Taskban</h1>
       </header>
+
       <main>
+        main
       </main>
     </div>
   );
