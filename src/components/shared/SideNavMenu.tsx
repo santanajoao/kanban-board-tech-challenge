671cc493cee4b9343c4cd825f38ab8f55e38bfb5
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-12-09 13:49:23 -0300

    build: changes mui to shadcn

--- src/components/shared/SideNavMenu.tsx
+++ src/components/shared/SideNavMenu.tsx
@@ -3,9 +3,9 @@
 import { IoMdClose } from 'react-icons/io';
 import { Board, Calendar, Timeline } from '@/components/icons';
 import { LiaThListSolid } from 'react-icons/lia';
-import { Drawer } from '@mui/material';
 import { k2d } from '@/fonts';
 import Link from 'next/link';
+import { Sheet, SheetContent } from '../ui/sheet';
 
 type Props = {
   closeMenu: () => void;
@@ -21,40 +21,42 @@ const navItems = [
 
 export function SideNavMenu({ closeMenu, isOpen }: Props) {
   return (
-    <Drawer anchor="left" open={isOpen} variant="temporary" onClose={closeMenu}>
-      <aside className="flex flex-col h-full w-fit bg-white px-2 py-3">
-        <button
-          type="button"
-          onClick={closeMenu}
-          className="text-2xl text-primaryPurple self-end"
-          aria-label="Fechar menu lateral"
-        >
-          <IoMdClose />
-        </button>
+    <Sheet open={isOpen}>
+      <SheetContent side="left">
+        <aside className="flex flex-col h-full w-fit bg-white px-2 py-3">
+          <button
+            type="button"
+            onClick={closeMenu}
+            className="text-2xl text-primaryPurple self-end"
+            aria-label="Fechar menu lateral"
+          >
+            <IoMdClose />
+          </button>
 
-        <div className="mt-16 h-full flex flex-col">
-          <span className={`text-3xl ${k2d.className} text-center text-primaryPurple`}>
-            Taskban
-          </span>
+          <div className="mt-16 h-full flex flex-col">
+            <span className={`text-3xl ${k2d.className} text-center text-primaryPurple`}>
+              Taskban
+            </span>
 
-          <nav className="mt-16">
-            <ul className="space-y-6">
-              {navItems.map(({ text, Icon, href }) => (
-                <li key={text}>
-                  <Link
-                    href={href}
-                    className="flex gap-x-5 py-2 px-14 w-full"
-                  >
-                    <Icon className="text-primaryPurple text-2xl" />
+            <nav className="mt-16">
+              <ul className="space-y-6">
+                {navItems.map(({ text, Icon, href }) => (
+                  <li key={text}>
+                    <Link
+                      href={href}
+                      className="flex gap-x-5 py-2 px-14 w-full"
+                    >
+                      <Icon className="text-primaryPurple text-2xl" />
 
-                    <span>{text}</span>
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </nav>
-        </div>
-      </aside>
-    </Drawer>
+                      <span>{text}</span>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </nav>
+          </div>
+        </aside>
+      </SheetContent>
+    </Sheet>
   );
 }
