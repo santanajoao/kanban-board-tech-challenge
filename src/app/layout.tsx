34c0563db24b51ed44351c0835759764415f42e4
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-12-12 18:23:45 -0300

    feat: add page metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,18 @@ import { ReactNode } from 'react';
 import { ModalProvider } from '@/contexts/ModalContext';
 import { OpenTaskCreationModal } from '@/components/shared/Modals/OpenTaskCreationModal';
 import { HeaderWithNavMenu } from '@/components/shared/HeaderWithNavMenu';
+import { Metadata } from 'next';
 import './globals.css';
+
+export const metadata: Metadata = {
+  title: 'Taskban',
+  description: 'Taskban é uma ferramenta de organização e gerenciamento de tarefas que funciona no seu computador ou celular. Taskban conta com diferentes tipos de visualização: quadro, lista, calendário e linha do tempo.',
+  keywords: ['kanban', 'quadro', 'organização', 'gerenciamento', 'tarefa', 'agilismo', 'projeto', 'prazos', 'lista', 'planejamento'],
+  authors: [
+    { name: 'santanajoao', url: 'https://github.com/santanajoao' },
+    { name: 'João Pedro Santana', url: 'https://www.linkedin.com/in/joaopedrosantanac/' },
+  ],
+};
 
 export default function RootLayout({ children }: {
   children: ReactNode,
