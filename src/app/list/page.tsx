08af963f32030bce04f913e667dfc431820d8229
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-12-13 00:36:43 -0300

    feat: add initial list renderization

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,5 +1,42 @@
+'use client';
+
+import { PriorityLabel } from '@/components/shared/PriorityLabel';
+import { useTasks } from '@/contexts/TaskContext';
+import { dateStringToDate } from '@/utils/date';
+
 export default function ListPage() {
+  const { taskLists } = useTasks();
+
+  const t = taskLists.flatMap((taskList) =>
+    taskList.tasks.map((task) =>({ ...task, taskListTitle: taskList.title })),
+  );
+
+  const tasks = [...t, ...t, ...t];
+
   return (
-    <div>List</div>
+    <div className="h-full p-1">
+      <table className="bg-white w-full h-fit overflow-auto">
+        <thead className="text-left">
+          <tr>
+            <th className="border py-1 px-2">Lista</th>
+            <th className="border py-1 px-2">Título</th>
+            <th className="border py-1 px-2">Priority</th>
+            <th className="border py-1 px-2">Data de entrega</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tasks.map((task) => (
+            <tr key={task.id}>
+              <td className="border py-1 px-2">{task.taskListTitle}</td>
+              <td className="border py-1 px-2">{task.title}</td>
+              <td className="border py-1 px-2">
+                <PriorityLabel className="text-sm" priority={task.priority} />
+              </td>
+              <td className="border py-1 px-2">{dateStringToDate(task.endDate).toLocaleDateString()}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
